2efd4544f9f84dfec59207810bde42713d372e70
Sam Jantz <sjantz0@gmail.com>
2017-05-06 17:32:30 +0000

    a more functional simple example

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM mhart/alpine-node:latest
 run mkdir /app
 add main.js /app/main.js
+expose 3000
 cmd node /app/main.js
--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Hello, world!")
+const http = require('http')
+const port = 3000
+
+const requestHandler = (request, response) => {
+  if(request.method == "POST") {
+    console.log("POST received to ", request.url)
+    console.log(request.body)
+  }
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
+
+//http://104.197.219.228:8080/sample
