2daa4d9be4b891d26cb58fe0f67ae5c401b8cfe9
Sam Jantz <sjantz0@gmail.com>
2017-05-08 21:52:50 -0500

    Doing more useful stuff like responding with a trolly image

--- main.js
+++ main.js
@@ -1,12 +1,66 @@
 const http = require('http')
-const port = 3000
+const default_resp = require('./default_resp.json')
+var port = 3000
+if (process.argv.length > 2) {
+  port = process.argv[2]
+}
 
 const requestHandler = function (request, response) {
   if(request.method == "POST") {
     console.log("POST received to ", request.url)
-    console.log(request.body)
+    var body = "";
+    request.on('data', function (chunk) {
+      body += chunk;
+    });
+    request.on('end', function () {
+      var contents = getContents(body)
+      console.log(contents.text)
+      console.log(decodeURIComponent(contents.response_url))
+      var fullUrl = decodeURIComponent(contents.response_url)
+      setTimeout(function(){
+        var postBody = JSON.stringify(picResponse())
+        console.log(postBody)
+        var contentLength = Buffer.byteLength(postBody)
+        var path = fullUrl.split("hooks.slack.com")
+        var option = {
+          host: "https://hooks.slack.com",
+          port: 443,
+          path: path[1],
+          method : 'POST',
+          headers : {
+            'Content-Type': 'application/json',
+            'Content-Length': contentLength
+          }
+        }
+        console.log(option)
+        var postReq = http.request(option, function(res){
+          console.log("Status from sending pic: ")
+          console.log(res.statusCode)
+        })
+      }, 1000)
+    })
   }
-  response.end('Hello Node.js Server!')
+  response.setHeader('content-type', 'application/json');
+  response.end(JSON.stringify(default_resp))
+}
+
+function getContents(body) {
+  var parts = body.split('&')
+  var contents = {}
+  parts.forEach(function(item) {
+    var pair = item.split("=")
+    contents[pair[0]] = pair[1]
+  }, this);
+  return contents
+}
+
+function picResponse() {
+  var pic = "http://i.imgur.com/hr3SX1Z.png"
+  var response = {
+    "response_type": "in_channel",
+    "text":pic
+  }
+  return response
 }
 
 const server = http.createServer(requestHandler)
@@ -16,7 +70,7 @@ server.listen(port, function (err) {
     return console.log('something bad happened', err)
   }
 
-  console.log(`server is listening on ${port}`)
+  console.log('server is listening on', port)
 })
 
 //http://104.197.219.228:8080
---
+++ make.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function build() {
+  echo "Building application"
+
+}
+
+action=$1
+if [ -z $action ] ; then 
+    action="build"
+fi
+case "$action" in
+  build)
+    build
+    ;;
+  serve)
+    node main.js
+    ;;
+  *)
+    echo unrecognized command "$action"
+    exit 1
+esac
